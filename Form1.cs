1f698c0c3e9672a24cfb8a515e15e04efac6d9b0
Dorian Marušić <dorian.marusic@skole.hr>
2022-04-07 17:22:14 +0200

    .

--- Form1.cs
+++ Form1.cs
@@ -1,8 +1,8 @@
 namespace ZavrsniRadSkola
 {
-    public partial class Form1 : Form
+    public partial class Form12 : Form
     {
-        public Form1()
+        public Form12()
         {
             InitializeComponent();
         }
